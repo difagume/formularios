f15137e5c35c0a06c2ab9d9140cbbe8ac7800010
Diego Fabricio <difagume@gmail.com>
2018-03-04 15:01:36 -0500

    Data: Agrupaciones de los objetos - formGroupName

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -12,10 +12,28 @@ export class DataComponent {
   // Responsable del manejo de la forma en su totalidad
   forma: FormGroup;
 
+  usuario: Object = {
+    nombrecompleto: {
+      nombre: 'diego',
+      apellido: 'guzman'
+    },
+    correo: 'difagume@gmail.com'
+  };
+
   constructor() {
+
+    console.log(this.usuario);
+
+
     this.forma = new FormGroup({
-      'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
-      'apellido': new FormControl('', Validators.required),
+
+      'nombrecompleto': new FormGroup({
+
+        'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
+        'apellido': new FormControl('', Validators.required)
+
+      }),
+
       'correo': new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')])
     });
   }
